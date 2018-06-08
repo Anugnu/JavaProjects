776428b7d5c5916a64c3fe76cd832a28a9ff1608
vinaykumarmadhasthu <vinay.work11@gmail.com>
2018-06-08 14:46:22 -0500

    Eighth commit of BankAccountApp

--- JavaProjects/src/bankaccountapp/BankAccountApp.java
+++ JavaProjects/src/bankaccountapp/BankAccountApp.java
@@ -1,9 +1,13 @@
 package bankaccountapp;
 
+import java.util.List;
+
 public class BankAccountApp {
 
 	public static void main(String[] args) {
+		String file = "D:\\NewBankAccounts.csv";
 		
+		/*
 		Checking chkacc1 = new Checking("Tim Wilson","321456879",1500 );
 		
 		Savings savacc1 = new Savings("Rich Love","456657897",2500);
@@ -11,11 +15,17 @@ public class BankAccountApp {
 		savacc1.showInfo();
 		System.out.println("*****************************");
 		chkacc1.showInfo();
-		
-		
+		*/
 		
 		// Read a CSV File then create new accounts based on that data
-
+		List<String[]> newAccountHolders = utilities.CSV.read(file);
+		for(String[] accountHolder : newAccountHolders) {
+			System.out.println("NEW ACCOUNT");
+			System.out.println(accountHolder[0]);
+			System.out.println(accountHolder[1]);
+			System.out.println(accountHolder[2]);
+			System.out.println(accountHolder[3]);
+		}
 	}
 
 }
