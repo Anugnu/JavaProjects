d6784dc94d443dcdc363e72ede13f515ab80d478
anugn <anugn@Anugnu-Dell>
2018-09-27 14:26:15 -0500

    Test success, reverting back the changes.

--- JavaProjects/src/bankaccountapp/Account.java
+++ JavaProjects/src/bankaccountapp/Account.java
@@ -70,8 +70,5 @@ public abstract class Account implements IBaseRate {
 				"\nBALANCE: " + balance
 				);
 	}
-	// this is test for making sure eclipse can change code into github.
-	public void test() {
-		
-	}
+	
 }
